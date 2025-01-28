c5e30028aa4f4518309aceb274b2c24af3186154
Kuruba <ksaipraveen6@gmail.com>
2025-01-28 18:19:39 +0530

    Python-Problem-1

--- python.py
+++ python.py
@@ -1,12 +1,14 @@
 def FindeachWordOccurance(a):
-    a.replace(' ','').lower()
-    seen=set()
-    result=[]
-    for i in a:
-        if i not in seen:
-            count=a.count(i)
-            result.append(f"{i.upper()}-{count}")
-            seen.add(i)
-    return result
+    text=a.replace(' ','').upper()
+    char_count={}
+    for i in text:
+        if i in char_count:
+            char_count[i]+=1
+        else:
+            char_count[i]=1
+    result= [f'{i}-{count}'for i,count in sorted(char_count.items())]
+    for i in result:
+        print(i)
     
-print(FindeachWordOccurance('Amolya Sharma'))
+    
+FindeachWordOccurance('Amolya Sharma')
